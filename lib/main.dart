6d519f90e9ed3e9c2809c337dfe3eb2d2ea97387
Zvi <zvi@zektec.com>
2019-03-28 14:50:08 +0200

    first working version. BUG in exporting to image

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mazifer',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
+        cursorColor: Colors.grey[800],
         textTheme: TextTheme(
           body1: TextStyle(
             fontFamily: 'Roboto Mono',
@@ -45,23 +50,21 @@ class _MaziferState extends State<Mazifer> {
     Color(0xff005792),
     Color(0xff74b49b),
     Color(0xff4a4a48),
-    Color(0xffffffff),
   ];
-  final PageController ctr = PageController();
-  Color bgColor = Color(0xff4a4a48);
-  Color mzColor = Color(0xff005792);
+  List<Color> selectedColors = [
+    Color(0xffff5733),
+    Color(0xff005792),
+  ];
+  final PageController pageCtr = PageController();
   double height = 200;
-
-  String text = "[io]";
+  String text = "";
+  GlobalKey globalKey = GlobalKey();
 
   void _rebuildMaze(String input) {
+    String temp = input.replaceAll(" ", "+").toLowerCase();
     setState(() {
-      text = "[" + input.replaceAll(" ", "+").toLowerCase() + "]";
+      text = temp == "" ? "" : "[" + temp + "]";
     });
-  }
-
-  void _changeColor(Color color) {
-    setState(() => bgColor = color);
   }
 
   void _zoom(ScaleUpdateDetails event) {
@@ -70,6 +73,11 @@ class _MaziferState extends State<Mazifer> {
     setState(() {
       height = max(min((200 * event.scale), 400), 40);
     });
+  }
+
+  void nextCard() {
+    pageCtr.nextPage(
+        duration: new Duration(milliseconds: 600), curve: Curves.easeIn);
   }
 
   Widget card(List<Widget> children) {
@@ -90,21 +98,35 @@ class _MaziferState extends State<Mazifer> {
     );
   }
 
-  Widget colorSector() {
+  Widget colorSector(int color) {
     return Container(
-      height: 42.0,
+      height: 46.0,
       child: ListView.builder(
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
         itemCount: colors.length,
         itemBuilder: (context, i) {
-          return Container(
-            width: 30,
-            height: 30,
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: colors[i],
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedColors[color] = colors[i];
+              });
+              nextCard();
+            },
+            child: Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: (colors[i] == selectedColors[color])
+                      ? Colors.black
+                      : colors[i],
+                  width: 3,
+                ),
+                color: colors[i],
+              ),
             ),
           );
         },
@@ -115,10 +137,7 @@ class _MaziferState extends State<Mazifer> {
   List<Widget> welcome() {
     return [
       FlatButton(
-        onPressed: () {
-          ctr.nextPage(
-              duration: new Duration(milliseconds: 600), curve: Curves.easeIn);
-        },
+        onPressed: nextCard,
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
@@ -138,8 +157,18 @@ class _MaziferState extends State<Mazifer> {
       Text("Entrer text to be mazified:"),
       TextField(
         onChanged: _rebuildMaze,
+        autofocus: true,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+        ),
+        style: Theme.of(context).textTheme.body2,
+        inputFormatters: <TextInputFormatter>[
+          LengthLimitingTextInputFormatter(24),
+          WhitelistingTextInputFormatter(RegExp("[a-z A-Z]")),
+        ],
         onSubmitted: (s) {
-          ctr.nextPage(
+          pageCtr.nextPage(
               duration: new Duration(milliseconds: 600), curve: Curves.easeIn);
         },
       ),
@@ -149,63 +178,81 @@ class _MaziferState extends State<Mazifer> {
   List<Widget> setMzColor() {
     return [
       Text("Set Maze Color:"),
-      colorSector(),
+      colorSector(0),
     ];
   }
 
   List<Widget> setBgColor() {
     return [
       Text("Set Background Color:"),
-      colorSector(),
+      colorSector(1),
     ];
   }
 
   List<Widget> share() {
     return [
-      Text("Share Your Maze!"),
+      FlatButton(
+        onPressed: () async {
+          RenderRepaintBoundary boundary =
+              globalKey.currentContext.findRenderObject();
+          ui.Image image = await boundary.toImage(pixelRatio: 5.0);
+          ByteData bytes = await image.toByteData(format: ui.ImageByteFormat.png);
+          await EsysFlutterShare.shareImage(
+              'myImageTest.png', bytes, 'my image title');
+        },
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              "Share your maze!",
+              style: Theme.of(context).textTheme.body2,
+            ),
+          ],
+        ),
+      ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
+      backgroundColor: selectedColors[1],
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset(
-                  'assets/images/logo.svg',
-                  height: 32,
-                  color: mzColor,
+              card([
+                Text(
+                  "Mayifer",
+                  style: Theme.of(context).textTheme.body2,
                 ),
-              ),
+              ]),
               Expanded(
                 child: GestureDetector(
                   onScaleUpdate: _zoom,
-                  child: CustomPaint(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: text.length,
-                      itemBuilder: (context, index) {
-                        return SvgPicture.asset(
-                          'assets/images/' + text[index] + '.svg',
-                          height: height,
-                          color: mzColor,
-                        );
-                      },
+                  child: RepaintBoundary(
+                    key: globalKey,
+                    child: Container(
+                      color: selectedColors[1],
+                      child: CustomPaint(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: text.length,
+                          itemBuilder: (context, index) {
+                            return SvgPicture.asset(
+                                'assets/images/' + text[index] + '.svg',
+                                height: height,
+                                color: selectedColors[0]);
+                          },
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              RaisedButton(
-                child: Text("to image"),
-                onPressed: null,
               ),
             ],
           ),
@@ -216,7 +263,7 @@ class _MaziferState extends State<Mazifer> {
           child: Container(
             height: 120,
             child: PageView(
-              controller: ctr,
+              controller: pageCtr,
               children: <Widget>[
                 card(welcome()),
                 card(setText()),
