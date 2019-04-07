d01567842ff1b30bf076f9566c0867fdc8602852
Zvi <zvi@zektec.com>
2019-04-07 23:38:30 +0300

    almost done ;)

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# mazifer
+# Mazifer
 
-A new Flutter project.
+A <5kb flutter app that creates word mazes!
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+**platform support:** Mazifer was tested on android devices only, but should be supported by ios too.
--- lib/main.dart
+++ lib/main.dart
@@ -11,228 +11,239 @@ import 'package:esys_flutter_share/esys_flutter_share.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mazifer',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        cursorColor: Colors.grey[800],
-        fontFamily: 'Yanone',
-        textTheme: TextTheme(
-          body1: TextStyle(fontSize: 24),
-          body2: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-        ),
-      ),
-      home: Mazifer(),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: 'Mazifer',
+theme: ThemeData(
+primarySwatch: Colors.indigo,
+fontFamily: 'Yanone',
+textTheme: TextTheme(
+body1: TextStyle(fontSize: 24),
+body2: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+),
+),
+home: Mazifer(),
+);
+}
 }
 
 class Mazifer extends StatefulWidget {
-  _MaziferState createState() => _MaziferState();
+_MaziferState createState() => _MaziferState();
 }
 
 class _MaziferState extends State<Mazifer> {
-  List<Color> c = Colors.primaries;
-  List<Color> cSelected = [Colors.red, Colors.indigo];
-  PageController pageCtr = PageController();
-  double height = 200;
-  String text = "[flutter]";
-  GlobalKey globalKey = GlobalKey();
+List<Color> c = Colors.primaries;
+PageController pageCtr = PageController();
+GlobalKey key = GlobalKey();
+TextEditingController txtCtr = TextEditingController();
+List<Color> selected;
+double height;
+String txt;
+bool done;
 
-  void _zoom(ScaleUpdateDetails event) {
-    if ((event.scale * event.horizontalScale * event.verticalScale) == 1)
-      return;
-    setState(() {
-      height = max(min((200 * event.scale), 400), 40);
-    });
-  }
+@override
+void initState() {
+super.initState();
+setup();
+}
 
-  void nextCard() {
-    pageCtr.nextPage(
-        duration: Duration(milliseconds: 600), curve: Curves.easeIn);
-  }
+void zoom(ScaleUpdateDetails e) {
+if ((e.scale * e.horizontalScale * e.verticalScale) == 1) return;
+setState(() => height = max(
+min((200 * e.scale), MediaQuery.of(context).size.height - 200), 40));
+}
 
-  Widget card(List<Widget> children) {
-    return Container(
-      margin: EdgeInsets.all(16),
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-        boxShadow: [
-          BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 3)
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: children,
-      ),
-    );
-  }
+void nextCard() {
+pageCtr.nextPage(
+duration: Duration(milliseconds: 600), curve: Curves.easeIn);
+}
 
-  Widget cSector(int color) {
-    return Container(
-      height: 56,
-      child: ListView.builder(
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        itemCount: c.length,
-        itemBuilder: (context, i) {
-          return GestureDetector(
-            onTap: () {
-              setState(() => cSelected[color] = c[i]);
-              nextCard();
-            },
-            child: Container(
-              width: 40,
-              margin: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  color: (c[i] == cSelected[color]) ? Colors.black : c[i],
-                  width: 3,
-                ),
-                color: c[i],
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
+void setup() {
+setState(() {
+selected = [Colors.red, Colors.indigo];
+txtCtr.text = "m";
+height = 200;
+txt = "[m]";
+done = false;
+});
+}
 
-  List<Widget> setText() {
-    return [
-      Text("Entrer text to be mazified:"),
-      TextField(
-        autofocus: true,
-        textAlign: TextAlign.center,
-        decoration: InputDecoration(
-          border: InputBorder.none,
-        ),
-        style: Theme.of(context).textTheme.body2,
-        inputFormatters: <TextInputFormatter>[
-          LengthLimitingTextInputFormatter(24),
-          WhitelistingTextInputFormatter(RegExp("[a-z A-Z]")),
-        ],
-        textInputAction: TextInputAction.next,
-        onEditingComplete: () {
-          SystemChannels.textInput.invokeMethod('TextInput.hide');
-          nextCard();
-        },
-        onChanged: (i) {
-          setState(() => text = "[" +
-              i.replaceAll(" ", "+").toLowerCase() +
-              "]".replaceAll("[]", ""));
-        },
-      ),
-    ];
-  }
+Widget card(List<Widget> children) {
+return Container(
+margin: EdgeInsets.all(16),
+padding: EdgeInsets.all(8),
+decoration: BoxDecoration(
+color: Colors.white,
+borderRadius: BorderRadius.circular(30),
+),
+child: Column(
+mainAxisAlignment: MainAxisAlignment.spaceAround,
+children: children,
+),
+);
+}
 
-  List<Widget> cPicker(int step) {
-    return [
-      Text(step == 2 ? "Set Maze Color:" : "Set Background Color:"),
-      cSector(step - 2),
-    ];
-  }
+Widget cSector(int step) {
+return Container(
+height: 56,
+child: ListView.builder(
+scrollDirection: Axis.horizontal,
+itemCount: c.length,
+itemBuilder: (context, i) {
+return GestureDetector(
+onTap: () {
+setState(() => selected[step] = c[i]);
+nextCard();
+},
+child: Container(
+width: 40,
+margin: EdgeInsets.all(8),
+decoration: BoxDecoration(
+borderRadius: BorderRadius.circular(30),
+border: Border.all(
+color: (c[i] == selected[step]) ? Colors.black : c[i],
+width: 3,
+),
+color: c[i],
+),
+),
+);
+},
+),
+);
+}
 
-  List<Widget> buttonCard(int step) {
-    return [
-      FlatButton(
-        onPressed: step == 0 ? nextCard : share,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              step == 0 ? "Get started" : "Share your maze",
-              style: Theme.of(context).textTheme.body2,
-            ),
-            Icon(step == 0 ? Icons.navigate_next : Icons.share),
-          ],
-        ),
-      ),
-    ];
-  }
+List<Widget> setText() {
+return [
+Text("Entrer text to be mazified:"),
+TextField(
+controller: txtCtr,
+autofocus: true,
+textAlign: TextAlign.center,
+decoration: InputDecoration(
+border: InputBorder.none,
+),
+style: Theme.of(context).textTheme.body2,
+inputFormatters: <TextInputFormatter>[
+LengthLimitingTextInputFormatter(10),
+WhitelistingTextInputFormatter(RegExp("[a-z A-Z]")),
+],
+textInputAction: TextInputAction.next,
+onEditingComplete: () {
+SystemChannels.textInput.invokeMethod('TextInput.hide');
+nextCard();
+},
+onChanged: (i) {
+setState(() => txt = "[" +
+i.replaceAll(" ", "+").toLowerCase() +
+"]".replaceAll("[]", ""));
+},
+),
+];
+}
 
-  void share() async {
-    RenderRepaintBoundary boundary =
-        globalKey.currentContext.findRenderObject();
-    ui.Image image = await boundary.toImage(pixelRatio: 5);
-    ByteData bytes = await image.toByteData(format: ui.ImageByteFormat.png);
-    await EsysFlutterShare.shareImage('maze.png', bytes, '');
-  }
+List<Widget> cPicker(int step) {
+return [
+Text(step == 2 ? "Set Maze Color:" : "Set Background Color:"),
+cSector(step - 2),
+];
+}
 
-  Widget maze() {
-    return Expanded(
-      child: GestureDetector(
-        onScaleUpdate: _zoom,
-        child: RepaintBoundary(
-          key: globalKey,
-          child: Container(
-            padding: EdgeInsets.only(top: 30),
-            decoration: BoxDecoration(
-              color: cSelected[1],
-              boxShadow: [
-                BoxShadow(color: Colors.black26, blurRadius: 5, spreadRadius: 3)
-              ],
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(30),
-              ),
-            ),
-            child: CustomPaint(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: text.length,
-                itemBuilder: (context, index) {
-                  return SvgPicture.asset(
-                      'assets/images/' + text[index] + '.svg',
-                      height: height,
-                      color: cSelected[0]);
-                },
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+List<Widget> button(int step) {
+return [
+FlatButton(
+onPressed: step == 0 ? nextCard : share,
+child: Row(
+mainAxisSize: MainAxisSize.min,
+children: <Widget>[
+Text(
+step == 0 ? "Get started" : "Share your maze",
+style: Theme.of(context).textTheme.body2,
+),
+Icon(step == 0 ? Icons.navigate_next : Icons.share),
+],
+),
+),
+];
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Center(
-                  child: Text("Mazifer",
-                      style: Theme.of(context).textTheme.body2)),
-            ),
-            maze(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Transform.translate(
-        offset: Offset(0, -1 * MediaQuery.of(context).viewInsets.bottom),
-        child: Container(
-          color: cSelected[1],
-          height: 130,
-          child: PageView(
-            controller: pageCtr,
-            children: [
-              buttonCard(0),
-              setText(),
-              cPicker(2),
-              cPicker(3),
-              buttonCard(4),
-            ].map((content) => card(content)).toList(),
-          ),
-        ),
-      ),
-    );
-  }
+void share() async {
+setState(() {
+height = 100;
+done = true;
+});
+RenderRepaintBoundary boundary = key.currentContext.findRenderObject();
+ui.Image img = await boundary.toImage(pixelRatio: 3);
+ByteData bytes = await img.toByteData(format: ui.ImageByteFormat.png);
+EsysFlutterShare.shareImage('m.png', bytes, '');
+}
+
+Widget maze() {
+return Center(
+child: GestureDetector(
+onScaleUpdate: zoom,
+child: SingleChildScrollView(
+scrollDirection: Axis.horizontal,
+child: RepaintBoundary(
+key: key,
+child: Container(
+color: selected[1],
+padding: EdgeInsets.all(16),
+child: CustomPaint(
+child: Row(
+children: txt
+.split("")
+.map(
+(t) => SvgPicture.asset(
+'assets/images/' + t + '.svg',
+height: height,
+color: selected[0],
+),
+)
+.toList(),
+),
+),
+),
+),
+),
+),
+);
+}
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+resizeToAvoidBottomPadding: false,
+backgroundColor: selected[1],
+body: maze(),
+bottomNavigationBar: Transform.translate(
+offset: Offset(0, -1 * MediaQuery.of(context).viewInsets.bottom),
+child: Container(
+height: 130,
+child: PageView(
+controller: pageCtr,
+children: [
+button(0),
+setText(),
+cPicker(2),
+cPicker(3),
+button(4),
+].map((item) => card(item)).toList(),
+),
+),
+),
+floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+floatingActionButton: done
+? FloatingActionButton(
+onPressed: () {
+pageCtr.jumpTo(0);
+setup();
+},
+child: Icon(Icons.replay, color: Colors.black),
+backgroundColor: Colors.white,
+)
+: null,
+);
+}
 }
